05da6948f86437b4e4013ea6580088d023cabe13
tchartchke <chartai.kl@gmail.com>
2020-12-17 11:20:54 -0600

    Done.

---
+++ db/migrate/20201217171803_create_dogs.rb
@@ -0,0 +1,23 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+
+  def down
+    drop_table :dogs
+  end
+end
+
+
+# class CreateDogs < ActiveRecord::Migration[5.2]
+#   def change
+#     create_table :dogs do |t|
+#       t.string :name
+#       t.string :breed
+#     end
+#   end
+
+# end
